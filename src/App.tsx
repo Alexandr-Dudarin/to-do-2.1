4d5f809faf72260e41fcae3e567abfa5b4ab63d2
Alexandr <stitel23@mail.ru>
2026-03-03 20:57:41 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ToDoListContainer from './components/ToDoListContainer';
+
+function App() {
+  return (
+    <div className="app">
+      <ToDoListContainer />
+    </div>
+  );
+}
+
+export default App;
